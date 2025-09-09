3f18bb044fd62e8754f9f7200face405318af793
David Kjellstrand <david.kjellstrand@gmail.com>
2025-09-09 11:28:05 +0200

    useEffect start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { getJobs } from "./services/jobServices";
 
 function App() {
-  return <></>;
+	// const [loading, setLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+
+	// useEffect(
+	// 	() =>
+	// 		async function fetchJobs() {
+	// 			try {
+	// 				const data: [] = await getJobs();
+	// 				console.log(data);
+	// 			} catch (error) {
+	// 				console.error("failed to fetch jobs", error);
+	// 			} finally {
+	// 				setLoading(false);
+	// 			}
+	// 			if ([].length === 0) {
+	// 				fetchJobs();
+	// 			} else {
+	// 				setLoading(false);
+	// 			}
+	// 		}
+	// );
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getJobs();
+      setJobs(data);
+    };
+ 
+    if (jobs.length > 0) return;
+
+    getData();
+  });
+
+
+	return (
+		<>
+			<h1>TEST</h1>
+		</>
+	);
 }
 
 export default App;
