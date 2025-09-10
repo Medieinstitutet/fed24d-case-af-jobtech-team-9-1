3200c7bbdf6ef6f2de43b88d902c48fb3a4bea7d
David Kjellstrand <david.kjellstrand@gmail.com>
2025-09-10 11:14:55 +0200

    Search Function in development

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,29 @@
-export const Search = ()  => {
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import { getJobs } from "../services/jobServices";
 
-    return<>Search</>
-}
+export const Search = () => {
+	const [searchWord, setSearchWord] = useState("");
+
+  // ON CHANGE
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchWord(e.target.value);
+	};
+
+  // ON SUBMIT
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+    getJobs(searchWord)
+	};
+
+	console.log(searchWord);
+
+	return (
+		<>
+			<h1>Search</h1>
+			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="sök" value={searchWord} onChange={handleChange} />
+				<button>Sök</button>
+			</form>
+		</>
+	);
+};
