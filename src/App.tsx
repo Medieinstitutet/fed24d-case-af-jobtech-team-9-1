c78ba4c0bae1032d694a143b8f59452be6fa2207
David Kjellstrand <david.kjellstrand@gmail.com>
2025-09-10 09:46:28 +0200

    Job service

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { getJobs } from './services/jobServices';
-import type { Jobs } from './models/Job';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getJob, getJobs } from "./services/jobServices";
+import type { Job, Jobs } from "./models/Job";
 
 function App() {
-  const [jobs, setJobs] = useState<Jobs>();
+	const [jobs, setJobs] = useState<Jobs>();
+	const [job, setJob] = useState<Job>();
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getJobs();
-      setJobs(data);
-    };
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getJobs();
+			setJobs(data);
+		};
 
-    if (jobs && jobs.hits.length > 0) return;
-    getData();
-  });
+		if (jobs && jobs.hits.length > 0) return;
+		getData();
+	});
 
-  console.log(jobs);
+	// console.log(jobs);
 
-  return (
-    <>
-      <h1>TEST</h1>
-      {jobs?.hits.map((job) => (
-        <div key={job.id}>
-          <h2>{job.headline}</h2>
-          <p>{job.description.text}</p>
-          <p>{job.application_deadline}</p>
-        </div>
-      ))}
-    </>
-  );
+	// GET JOB BY ID
+
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getJob();
+			setJob(data);
+		};
+
+		if (job) return;
+		getData();
+	});
+
+	console.log(job);
+
+	return (
+		<>
+			<h1>TEST</h1>
+			{jobs?.hits.map((job) => (
+				<div key={job.id}>
+					<h2>{job.headline}</h2>
+					<p>{job.description.text}</p>
+					<p>{job.application_deadline}</p>
+				</div>
+			))}
+      <h1>{job?.headline}</h1>
+      <h3>{job?.description.text}</h3>
+		</>
+	);
 }
 
 export default App;
--- src/services/jobServices.ts
+++ src/services/jobServices.ts
@@ -1,11 +1,22 @@
-import type { Jobs } from "../models/Job";
+import type { Job, Jobs } from "../models/Job";
 
-const API_URL = "https://jobsearch.api.jobtechdev.se/search?q=java&offset=0&limit=10";
+const Q = "java"
+const API_URL = `https://jobsearch.api.jobtechdev.se/search?q=${Q}&offset=0&limit=10`;
+const API_URL_JOB = "https://jobsearch.api.jobtechdev.se/ad/";
+const ID = "29958628";
 
 export async function getJobs(): Promise<Jobs> {
-  const response = await fetch(API_URL);
-  if(!response.ok) {
-    throw new Error("Failed to fetch");
-  }
-  return response.json();
+	const response = await fetch(API_URL);
+	if (!response.ok) {
+		throw new Error("Failed to fetch");
+	}
+	return response.json();
+}
+
+export async function getJob(): Promise<Job> {
+	const response = await fetch(`${API_URL_JOB}${ID}`);
+	if (!response.ok) {
+		throw new Error("Failed to fetch specific job");
+	}
+	return response.json();
 }
