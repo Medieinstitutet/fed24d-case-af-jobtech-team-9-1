8d189323b38fd62557825668348d4ad22e7029f8
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2025-09-11 09:26:05 +0200

    Switch search form to digiform

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { JobList } from "../components/JobList"
-import { Search } from "../components/Search"
+import { JobList } from '../components/JobList';
+import { Search } from '../components/Search';
 
 export const Home = () => {
-    return<>
-    Home
-        <Search/>
-        <JobList/>
+  return (
+    <>
+      <Search />
+      <JobList />
     </>
-}
+  );
+};
