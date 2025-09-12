85c6613f23c53331542395559ec9c66768783f1d
David Kjellstrand <david.kjellstrand@gmail.com>
2025-09-12 12:59:41 +0200

    Job details with Qualifications component

--- src/pages/JobDetails.tsx
+++ src/pages/JobDetails.tsx
@@ -5,8 +5,11 @@ import { Link, useParams } from "react-router";
 import {
 	DigiInfoCard,
 	DigiLayoutBlock,
+	DigiLayoutContainer,
+	DigiLayoutMediaObject,
 	DigiLinkInternal,
 	DigiMediaImage,
+	DigiTypographyTime,
 } from "@digi/arbetsformedlingen-react";
 import {
 	InfoCardHeadingLevel,
@@ -14,8 +17,12 @@ import {
 	InfoCardType,
 	InfoCardVariation,
 	LayoutBlockVariation,
+	LayoutContainerVariation,
+	LayoutMediaObjectAlignment,
 	LinkVariation,
+	TypographyTimeVariation,
 } from "@digi/arbetsformedlingen";
+import { Qualifications } from "../components/Qualifications";
 
 export const JobDetails = () => {
 	const [job, setJob] = useState<Job>();
@@ -34,31 +41,77 @@ export const JobDetails = () => {
 
 	return (
 		<>
-			<DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+			<DigiLayoutContainer afVerticalPadding afVariation={LayoutContainerVariation.STATIC}>
 				<Link to={"/"}>
-					<DigiLinkInternal af-variation={LinkVariation.SMALL}>
+					<DigiLinkInternal af-variation={LinkVariation.LARGE}>
 						Sökresultat
 					</DigiLinkInternal>
 				</Link>
+			</DigiLayoutContainer>
+			<DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+				<DigiLayoutMediaObject afAlignment={LayoutMediaObjectAlignment.START}>
+					<DigiMediaImage
+						// slot="media"
+						// afFullwidth
+						// afWidth="100"
+						afHeight="100"
+						afUnlazy
+						afSrc={`${job?.logo_url}`}
+						af-alt={`${job?.employer.name}`}
+					></DigiMediaImage>
+				</DigiLayoutMediaObject>
 				<h1>{job?.headline}</h1>
-				<DigiMediaImage
-					afUnlazy
-					afHeight="100"
-					afWidth="100"
-					afSrc={`${job?.logo_url}`}
-					af-alt={`${job?.employer.name}`}
-				></DigiMediaImage>
 				<h2>{job?.employer.name}</h2>
-				<h3>{job?.occupation.label}</h3>
-				<p>{job?.occupation_field.label}</p>
-				<p>{job?.working_hours_type.label}</p>
-				<p>{job?.duration.label}</p>
-				<p>{job?.workplace_address.country}</p>
-				<p>{job?.workplace_address.region}</p>
-				<p>{job?.workplace_address.country}</p>
-				<p>Lediga tjänster: {job?.number_of_vacancies}</p>
-				<p>{job?.salary_type.label}</p>
-				<p>Sök senast: {job?.application_deadline}</p>
+				<h4>{job?.occupation.label}</h4>
+				<h4>Komun: {job?.workplace_address.municipality}</h4>
+			</DigiLayoutBlock>
+			<DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+				<DigiLayoutContainer afNoGutter>
+					<span>Omfattning: </span>
+					<span>{job?.working_hours_type.label}</span>
+				</DigiLayoutContainer>
+				<DigiLayoutContainer afNoGutter>
+					<span>Varaktighet: </span>
+					<span>{job?.duration.label}</span>
+				</DigiLayoutContainer>
+				<DigiLayoutContainer afNoGutter>
+					<span>Lediga tjänster: </span>
+					<span>{job?.number_of_vacancies}</span>
+				</DigiLayoutContainer>
+			</DigiLayoutBlock>
+
+			{/* QUALIFICATIONS HÄR*/}
+			<Qualifications job={job}></Qualifications>
+
+			<DigiLayoutBlock afMarginTop afVariation={LayoutBlockVariation.PRIMARY}>
+				<DigiInfoCard
+					af-heading="Beskrivning"
+					af-heading-level={InfoCardHeadingLevel.H3}
+					afSize={InfoCardSize.STANDARD}
+					afType={InfoCardType.RELATED}
+					afLinkHref={job?.webpage_url}
+					afLinkText="Till jobbet"
+					afVariation={InfoCardVariation.SECONDARY}
+				>
+					<div
+						dangerouslySetInnerHTML={{
+							__html: job?.description.text_formatted ?? "",
+						}}
+					/>
+				</DigiInfoCard>
+			</DigiLayoutBlock>
+			<DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+				<ul>
+					<li>{job?.occupation_field.label}</li>
+					<li>{job?.workplace_address.country}</li>
+					<li>{job?.workplace_address.region}</li>
+					<li>{job?.workplace_address.country}</li>
+					<li>{job?.salary_type.label}</li>
+				</ul>
+				<DigiTypographyTime
+					afVariation={TypographyTimeVariation.DISTANCE}
+					afDateTime={job?.application_deadline}
+				></DigiTypographyTime>
 				<p>
 					Körkort:
 					{job?.driving_license_required === true
@@ -75,21 +128,6 @@ export const JobDetails = () => {
 						? " Nej"
 						: " Ej angivet"}
 				</p>
-				<DigiInfoCard
-					af-heading="Beskrivning"
-					afSize={InfoCardSize.STANDARD}
-					af-heading-level={InfoCardHeadingLevel.H3}
-					afType={InfoCardType.TIP}
-					afLinkHref={job?.webpage_url}
-					afLinkText="Till jobbet"
-					afVariation={InfoCardVariation.PRIMARY}
-				>
-					<div
-						dangerouslySetInnerHTML={{
-							__html: job?.description.text_formatted ?? "",
-						}}
-					/>
-				</DigiInfoCard>
 			</DigiLayoutBlock>
 		</>
 	);
