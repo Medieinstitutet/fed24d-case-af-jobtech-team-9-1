cdb08187811ef04ffef0847ad371aacf08423551
David Kjellstrand <david.kjellstrand@gmail.com>
2025-09-17 10:43:40 +0200

    Styled footer and header

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,24 +1,13 @@
-import { Link, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import { JobContext } from "../context/JobContext";
 import { useReducer } from "react";
 import { initialJobState, JobReducer } from "../reducers/JobReducer";
 import {
-	DigiFooter,
-	DigiFooterCard,
-	DigiHeader,
-	DigiIconAccessibilityUniversal,
-	DigiIconEnvelope,
-	DigiIconExternalLinkAlt,
-	DigiIconGlobe,
-	DigiIconSign,
-	DigiLogo,
+	DigiLayoutContainer,
 	DigiTypography,
 } from "@digi/arbetsformedlingen-react";
 import {
-	FooterVariation,
-	FooterCardVariation,
-	LogoVariation,
-	LogoColor,
+  LayoutContainerVariation,
 } from "@digi/arbetsformedlingen";
 
 export const Layout = () => {
@@ -27,119 +16,15 @@ export const Layout = () => {
 		<>
 			<JobContext.Provider value={{ ...state, dispatch }}>
 				<DigiTypography>
-					<DigiHeader
-						afSystemName="Designsystem"
-						afHideSystemName={false}
-						afMenuButtonText="Meny"
-					></DigiHeader>
+          <DigiLayoutContainer afVerticalPadding afVariation={LayoutContainerVariation.FLUID} className="header">
+            <h1>SÖK JOBB</h1>
+          </DigiLayoutContainer>
 					<main>
 						<Outlet />
 					</main>
-					<DigiFooter afVariation={FooterVariation.LARGE}>
-						<div slot="content-top">
-							<div>
-								<DigiFooterCard afType={FooterCardVariation.ICON}>
-									<ul>
-										<li>
-											<a href="#">
-												<DigiIconAccessibilityUniversal></DigiIconAccessibilityUniversal>
-												Tillgänglighetsredogörelse
-											</a>
-										</li>
-										<li>
-											<a href="#">
-												<DigiIconSign></DigiIconSign>
-												Teckenspråk
-											</a>
-										</li>
-										<li>
-											<a href="#">
-												<DigiIconGlobe></DigiIconGlobe>
-												Other languages
-											</a>
-										</li>
-										<li>
-											<a href="#">
-												<DigiIconEnvelope></DigiIconEnvelope>
-												Mejla vår funktionbrevlåda
-											</a>
-										</li>
-									</ul>
-								</DigiFooterCard>
-							</div>
-							<div>
-								<DigiFooterCard>
-									<ul>
-										<li>
-											<a href="#">Tillgänglighetsredogörelse</a>
-										</li>
-										<li>
-											<a href="#">Teckenspråk</a>
-										</li>
-										<li>
-											<a href="#">Other languages</a>
-											<br />
-										</li>
-										<li>
-											<a href="#">Mejla vår funktionbrevlåda</a>
-										</li>
-										<li>
-											<a href="#">Mejla vår funktionbrevlåda</a>
-										</li>
-										<li>
-											<a href="#">Mejla vår funktionbrevlåda</a>
-										</li>
-									</ul>
-								</DigiFooterCard>
-							</div>
-							<div>
-								<DigiFooterCard afType={FooterCardVariation.ICON}>
-									<h2>Fokus abacus dolores</h2>
-									<p>
-										Vivamus feugiat nunc vel enim fermentum dolores ab. Nuj vel
-										enim fermentum dolores abacus. Vivanna helam conkista
-									</p>
-									<a href="#">
-										<DigiIconExternalLinkAlt></DigiIconExternalLinkAlt>
-										Mejla vår funktionbrevlåda
-									</a>
-								</DigiFooterCard>
-							</div>
-							<div>
-								<DigiFooterCard afType={FooterCardVariation.BORDER}>
-									<a href="#">Forium dolores</a>
-									<p>
-										Vivamus feugiat nunc vel enim fermentum, ac aliquet tortor.
-									</p>
-								</DigiFooterCard>
-								<DigiFooterCard afType={FooterCardVariation.BORDER}>
-									<a href="#">Leveante lipsum</a>
-									<p>Vivamus feugiat nunc vel enim.</p>
-								</DigiFooterCard>
-								<DigiFooterCard afType={FooterCardVariation.BORDER}>
-									<a href="#">Acta estium</a>
-									<p>
-										fermentum, ac aliquet tortor cursus. Curabitur cursus eros.
-									</p>
-								</DigiFooterCard>
-							</div>
-						</div>
-						<div slot="content-bottom-left">
-							<Link to="/">
-								<DigiLogo
-									afVariation={LogoVariation.LARGE}
-									afColor={LogoColor.SECONDARY}
-								></DigiLogo>
-							</Link>
-						</div>
-						<div slot="content-bottom-right">
-							<p>Följ oss på</p>
-							<a href="#">Facebook</a>
-							<a href="#">Youtube</a>
-							<a href="#">Linkedin</a>
-							<a href="#">Instagram</a>
-						</div>
-					</DigiFooter>
+					<DigiLayoutContainer afVerticalPadding afVariation={LayoutContainerVariation.FLUID} className="footer">
+            <h4>Footer</h4>
+          </DigiLayoutContainer>
 				</DigiTypography>
 			</JobContext.Provider>
 		</>
