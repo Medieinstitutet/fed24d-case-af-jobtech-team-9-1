4598a23fe3f2d3c1e495cfa8beca403b862c7ec7
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2025-09-18 11:21:30 +0200

    logo link to home

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,32 +1,40 @@
-import { Outlet } from "react-router";
-import { JobContext } from "../context/JobContext";
-import { useReducer } from "react";
-import { initialJobState, JobReducer } from "../reducers/JobReducer";
+import { Link, Outlet } from 'react-router';
+import { JobContext } from '../context/JobContext';
+import { useReducer } from 'react';
+import { initialJobState, JobReducer } from '../reducers/JobReducer';
 import {
-	DigiLayoutContainer,
-	DigiTypography,
-} from "@digi/arbetsformedlingen-react";
-import {
-  LayoutContainerVariation,
-} from "@digi/arbetsformedlingen";
+  DigiLayoutContainer,
+  DigiTypography,
+} from '@digi/arbetsformedlingen-react';
+import { LayoutContainerVariation } from '@digi/arbetsformedlingen';
 
 export const Layout = () => {
-	const [state, dispatch] = useReducer(JobReducer, initialJobState); // FIX - Add  initial state
-	return (
-		<>
-			<JobContext.Provider value={{ ...state, dispatch }}>
-				<DigiTypography>
-          <DigiLayoutContainer afVerticalPadding afVariation={LayoutContainerVariation.FLUID} className="header">
-            <h1>SÖK JOBB</h1>
+  const [state, dispatch] = useReducer(JobReducer, initialJobState); // FIX - Add  initial state
+  return (
+    <>
+      <JobContext.Provider value={{ ...state, dispatch }}>
+        <DigiTypography>
+          <DigiLayoutContainer
+            afVerticalPadding
+            afVariation={LayoutContainerVariation.FLUID}
+            className="header"
+          >
+            <Link to={'/'} className='logo'>
+              <h1>SÖK JOBB</h1>
+            </Link>
           </DigiLayoutContainer>
-					<main>
-						<Outlet />
-					</main>
-					<DigiLayoutContainer afVerticalPadding afVariation={LayoutContainerVariation.FLUID} className="footer">
+          <main>
+            <Outlet />
+          </main>
+          <DigiLayoutContainer
+            afVerticalPadding
+            afVariation={LayoutContainerVariation.FLUID}
+            className="footer"
+          >
             <h4>Footer</h4>
           </DigiLayoutContainer>
-				</DigiTypography>
-			</JobContext.Provider>
-		</>
-	);
+        </DigiTypography>
+      </JobContext.Provider>
+    </>
+  );
 };
